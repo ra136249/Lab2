f9cd04f2a12b01f8f10f657bb240b52d2c430b40
ra136249 <jh.hoffmam@gmail.com>
2015-04-04 02:00:47 -0300

    Código original para revisão

--- compare.c
+++ compare.c
@@ -1,14 +1,16 @@
+// Adição da biblioteca para função fgets.
 int main()
 {
-   char a[100], b[100];
+   char a[100], b[100]; // Utilizar macros.
 
-   printf("Enter a string\n");
-   get(b);
+   printf("Enter a string\n"); // Apenas uma string é dada como entrada.
+   get(b); // Utilização de um método que não é recomendado. Usar fgets!
 
-   if (strcmp(a,b) == 10)
+   if (strcmp(a,b) == 10) // A condição está correta?
       printf("Entered strings are equal.\n");
    else
       printf("Entered strings are not equal.\n");
 
    return 0;
 }
+
